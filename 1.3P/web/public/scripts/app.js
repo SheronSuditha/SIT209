e638aab0b69a8f2556825cfca9724982c505fd7a
Sheron Suditha <suditha@gamer.lk>
2020-07-18 00:04:41 +0530

    ENCHANCE: Registeration logic updated

--- 1.3P/web/public/scripts/app.js
+++ 1.3P/web/public/scripts/app.js
@@ -2,6 +2,7 @@ $('#navbar').load('navbar.html')
 $('#footer').load('footer.html')
 
 const devices = JSON.parse(localStorage.getItem('devices')) || []; //if no devices returns an empty array
+const local_users = JSON.parse(localStorage.getItem('users')) || [];
 
 /**
  * using jquery
@@ -37,6 +38,76 @@ $('#send-command').on('click', function () {
     const command = $('#command').val();
     console.log(`command is ${command}`)
 })
+
+
+/**
+ * registeration hook
+ */
+
+$('#register').on('click', handle_register);
+
+function handle_register() {
+    const username = $('#user').val();
+    const password = $('#password').val();
+    const password_confirmation = $('#cpassword').val();
+
+    if (!username || !password) {
+        $('#alert').show();
+        return $('#alert').text("Please check if the username or the password is entered")
+    }
+
+    if (password !== password_confirmation) {
+        $('#alert').show();
+        return $('#alert').text("Entered passwords does not match.")
+    }
+
+    console.log(check_for_user(username, password))
+
+    if (check_for_user(username, password) === true) {
+        $('#alert').removeClass("alert-success")
+        $('#alert').addClass("alert-danger")
+        $('#alert').show();
+        return $('#alert').text("Already Registered")
+    } else {
+        $('#alert').show();
+        $('#alert').removeClass("alert-danger")
+        $('#alert').addClass("alert-success")
+        $('#alert').text("Success!")
+        local_users.push({
+            username,
+            password
+        })
+        localStorage.setItem('users', JSON.stringify(local_users))
+    }
+}
+
+function check_for_user(username, password) {
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    let usrr = users.find(usr => usr.username === username && usr.password === password)
+    if (usrr) {
+        return true;
+    } else {
+        return false
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /**
  * using basic js for DOM manipulation.
