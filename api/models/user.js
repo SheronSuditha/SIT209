8b95855b841ec17e5cd35a9e8ca63522629a067e
SheronSuditha <github@sudithakumarasinghe.me>
2020-08-17 16:52:24 +0530

    QUALITY: Branch cleanup

--- api/models/user.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-module.exports = mongoose.model('User', new mongoose.Schema({
-    id: String,
-    name: String,
-    password: String,
-    isAdmin: Boolean
-}));
